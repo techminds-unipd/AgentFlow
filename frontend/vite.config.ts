82382994475e3700101a6fba06e26eb10794f92e
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-02-27 17:15:56 +0100

    Dockerizzato tutto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
-    strictPort: true, // not necessary
-    port: 80, // you can replace this port with any port
+    port: 5173, // you can replace this port with any port
   }
 })
