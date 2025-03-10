8750b17022bb200cbfee383f277abdf20a7cd892
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-10 19:00:38 +0100

    feat: codifica+test footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,44 @@
+import {Link, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import '../../index.css'
+
 export const Footer=()=>{
     return(
-        <>Hello footer</>
+        <Grid 
+            container  
+            className='footer' 
+            textAlign={'center'}
+            bottom={0}
+            width={'100%'}
+            position={'fixed'}
+            padding={1.75}
+            sx={{
+                backgroundColor: 'var(--maincolor)',
+                color: 'var(--white-text)'
+            }}
+            >
+            <Grid 
+                size={'grow'} 
+                display='flex' 
+                gap={1} 
+                justifyContent='center' 
+                alignItems={'center'}
+                >
+                <GitHubIcon fontSize='small'/>
+                <Link 
+                    href='https://github.com/techminds-unipd' 
+                    target='_blank' 
+                    sx={{
+                        color: 'var(--white-text)', 
+                        textDecoration: 'underline var(--white-text)' 
+                    }}>
+                        GitHub repository
+                </Link>
+            </Grid>
+            <Grid size={'grow'}>
+                <Typography>Made by Tech Minds</Typography>
+            </Grid>
+        </Grid>
     )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -10,5 +11,12 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     port: 5173, // you can replace this port with any port
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   }
 })
+
+
+  
