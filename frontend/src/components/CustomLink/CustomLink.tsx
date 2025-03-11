f85d37522e7e76e69e21c85b0540081b3b261599
cvallott <caterina.vallotto@studenti.unipd.it>
2025-03-11 10:51:10 +0100

    #14 componente CustomLink

--- frontend/src/components/CustomLink/CustomLink.tsx
+++ frontend/src/components/CustomLink/CustomLink.tsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router";
 import { Link, Typography } from '@mui/material';
+import {
+    Link as RouterLink,
+    LinkProps as RouterLinkProps,
+  } from 'react-router';
+import React from "react";
 
 interface CustomLinkProps {
     name: string,
@@ -6,9 +12,21 @@ interface CustomLinkProps {
 }
 
 export const CustomLink=( {name, link}: CustomLinkProps) =>{
+    const {pathname} = useLocation();
+    const LinkBehavior = React.forwardRef<any, Omit<RouterLinkProps, 'to'>>(
+        (props, ref) => (
+          <RouterLink
+            ref={ref}
+            to={link}
+            {...props}
+          />
+        ),
+      );
     return(
         <>
-            {name} {link}
+            {pathname===link?<Typography>{name}</Typography>:
+            <Link component={LinkBehavior} underline='always' color={'warning'}>{name}</Link>
+            }
         </>
     )
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import {CustomLink} from '../CustomLink/CustomLink.tsx'
+
 export const Navbar=()=>{
     return(
         <>
             Hello Navbar
-            <CustomLink props={'ciao', 'ciao'}/>
+            <CustomLink name='Home' link='/'/>
+            <CustomLink name='About Us' link='/aboutus'/>
         </>
     )
 }
