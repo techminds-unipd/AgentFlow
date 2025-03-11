6c75be61ccb92c01ebb1071acbb5d24f4f076b38
leosalvi03 <leonardosalviato03@gmail.com>
2025-03-11 10:24:25 +0100

    Inizio sviluppo navbar #14

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import {Link, Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import '../../index.css'
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
+import {CustomLink} from '../CustomLink/CustomLink.tsx'
 export const Navbar=()=>{
     return(
         <>
             Hello Navbar
+            <CustomLink props={'ciao', 'ciao'}/>
         </>
     )
 }
