7fd026061aabf6705fa5f63fc003d00c866b2f1a
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-11 12:13:03 +0100

    Aggiunto commento funzionamento CustomLink

--- frontend/src/components/CustomLink/CustomLink.tsx
+++ frontend/src/components/CustomLink/CustomLink.tsx
@@ -11,6 +11,12 @@ interface CustomLinkProps {
     link: string
 }
 
+/**
+ * Componente custom che permette di avere link non circolari. Quando il percorso che viene passato come link
+ * è uguale al percorso di React Router in cui si trova il componente, allora viene ritornato un semplice testo con Typography.
+ * La funzione LinkBehaviour è stata presa da https://mui.com/material-ui/integrations/routing/
+ */
+
 export const CustomLink=( {name, link}: CustomLinkProps) =>{
     const {pathname} = useLocation();
     const LinkBehavior = React.forwardRef<HTMLAnchorElement, Omit<RouterLinkProps, 'to'>>(
