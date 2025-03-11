4cd54044617dfd76178bb0de75eafa6ddd3498ba
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-11 15:54:36 +0100

    Fix test

--- frontend/src/components/CustomButton/CustomButton.test.tsx
+++ frontend/src/components/CustomButton/CustomButton.test.tsx
@@ -2,41 +2,40 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { expect, test, describe } from "vitest";
 import { MemoryRouter } from "react-router";
-import userEvent from "@testing-library/user-event";
 import { CustomButton } from "./CustomButton";
 
 describe("CustomButton", () => {
+  const buttonLink = "/test";
+  const buttonText = "Button Name";
 
   test("Renders button with correct text", () => {
     render(
       <MemoryRouter>
-        <CustomButton name="Button Name" link="/test" variant="contained" />
+        <CustomButton name={buttonText} link={buttonLink} variant="contained" />
       </MemoryRouter>
     );
 
-    expect(screen.getByRole("link", { name: /button name/i })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: buttonText })).toBeInTheDocument();
   });
 
   test("Renders button has the correct link", () => {
     render(
       <MemoryRouter>
-        <CustomButton name="Button Name" link="/test" variant="contained" />
+        <CustomButton name={buttonText} link={buttonLink} variant="contained" />
       </MemoryRouter>
     );
 
-    expect(screen.getByRole("link", { name: /button name/i })).toHaveAttribute("href", "/test");
+    expect(screen.getByRole("link", { name: buttonText })).toHaveAttribute("href", buttonLink);
   });
 
   test("Renders button navigation on click", async () => {
     render(
       <MemoryRouter>
-        <CustomButton name="Button Name" link="/test" variant="contained" />
+        <CustomButton name={buttonText} link={buttonLink} variant="contained" />
       </MemoryRouter>
     );
 
-    const button = screen.getByRole("link", { name: /button name/i });
-    await userEvent.setup().click(button);
-
-    expect(button).toHaveAttribute("href", "/test");
+    const button = screen.getByRole("link", { name: buttonText });
+    expect(button).toHaveAttribute("href", buttonLink);
   });
 });
