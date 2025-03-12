e3b44502e766f7338d9b46083840b2cb67075b61
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-12 16:25:26 +0100

    Inizio test

---
+++ frontend/src/pages/AboutUs/AboutUs.test.tsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { expect, test, describe, assert } from "vitest";
+import { AboutUs } from "./AboutUs";
+
+describe("AboutUs section test", ()=>{
+    test("Logo is present", ()=>{
+        render(<AboutUs/>);
+        expect(screen.getByAltText("Tech Minds logo")).toBeInTheDocument();
+    });
+    test("6 PersonCards are present", ()=>{
+        render(<AboutUs/>);
+        const personCards = screen.getAllByRole("img");
+        assert(personCards.length>=7);
+    });
+    test("Link to documentation is present", ()=>{
+        render(<AboutUs/>);
+        let found: boolean = false;
+        const linksArray = screen.getAllByRole("link");
+        
+        assert(found);
+    });
+})
