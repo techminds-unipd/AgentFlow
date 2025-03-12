0eaa6457c0979d2d1d9ea8096f57df0e44d21438
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-03-12 12:09:10 +0100

    Fix WorkflowNotFoundError message in Controller #8

--- backend/src/workflow/adapter/input/DeleteWorkflowController.ts
+++ backend/src/workflow/adapter/input/DeleteWorkflowController.ts
@@ -34,7 +34,7 @@ class DeleteWorkflowController {
             );
             return this.toDTO(workflow);
         } catch (error) {
-            if (error instanceof WorkflowNotFoundError) throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            if (error instanceof WorkflowNotFoundError) throw new HttpException("Workflow not found", HttpStatus.BAD_REQUEST);
             throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
