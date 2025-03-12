c96b79d172a17e7794874c338291bb39a737cf7a
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-12 15:56:07 +0100

    Aggiunto il componente About Us

--- frontend/src/pages/AboutUs/AboutUs.tsx
+++ frontend/src/pages/AboutUs/AboutUs.tsx
@@ -1,5 +1,63 @@
-export const AboutUs=()=>{
-    return(
-        <>About us</>
-    )
-}
+import { PersonCard } from "../../components/PersonCard/PersonCard";
+import Grid from "@mui/material/Grid2";
+import { Typography, Link } from "@mui/material";
+import "./AboutUs.css";
+
+export const AboutUs = () => {
+  return (
+    <main>
+      <Grid container>
+        <Grid container justifyContent="center" size={12} marginBottom={4}>
+          <img src="/Logo-TechMinds.png" />
+        </Grid>
+        <Grid
+          container
+          gap={6}
+          alignItems="center"
+          justifyContent="center"
+          width={"100%"}
+          marginTop={3}
+        >
+          <Grid>
+            <PersonCard name="Alessandro Bressan" GitHubUsername="T-brex" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Samuele Corradin" GitHubUsername="Jacky8703" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Tommaso Lazzarin" GitHubUsername="tlazzarin" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Leonardo Salviato" GitHubUsername="leosalvi03" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Matteo Squarzoni" GitHubUsername="MatteoSquarz" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Giuseppe Tutino" GitHubUsername="Giuseppe5000" />
+          </Grid>
+          <Grid>
+            <PersonCard name="Caterina Vallotto" GitHubUsername="cvallott" />
+          </Grid>
+        </Grid>
+        <Grid size={12} marginLeft={10} marginRight={10} paddingTop={7}>
+          <Typography textAlign={"center"}>
+            This website is made by Tech Minds, a passionate SWE group from the
+            University of Padova. You can find our GitHub profile{" "}
+            <Link
+              target="_blank"
+              href="https://github.com/techminds-unipd"
+              sx={{
+                color: "var(--maincolor)",
+                textDecoration: "underline var(--maincolor)",
+              }}
+            >
+              here
+            </Link>
+            .
+          </Typography>
+        </Grid>
+      </Grid>
+    </main>
+  );
+};
