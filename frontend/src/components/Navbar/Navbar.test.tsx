388cd7457c01e1c0a11d10863087ae5674db5c87
T-Brex <bressan816@gmail.com>
2025-03-12 17:01:50 +0100

    aggiunta Navbar test (da ricontrollare)

--- frontend/src/components/Navbar/Navbar.test.tsx
+++ frontend/src/components/Navbar/Navbar.test.tsx
@@ -1,20 +1,37 @@
-import "@testing-library/jest-dom";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import { expect, test, describe, vi } from "vitest";
-import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
+import {expect, test, describe} from 'vitest'
+import { Navbar } from './Navbar'
 import { MemoryRouter } from "react-router";
-import { Navbar } from "./Navbar";
 
-describe("Navbar", () => {
-  test("Renders logout menu item", () => {
-    const fn = vi.fn();
+describe('Navbar', () => {
+  
+  test('Renders the navbar', () => {
     render(
       <MemoryRouter>
         <Navbar />
       </MemoryRouter>
     );
-
-    expect(cio.getByRole("navbar")).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toBeInTheDocument()
   });
-    
+
+  test('Renders the home item', () => {
+    render(
+      <MemoryRouter>
+          <Navbar />
+      </MemoryRouter>
+  );
+      expect(screen.getByText('Home')).toBeInTheDocument()
+  });
+  
+  test('Renders the about us item', () => {
+    render(
+      <MemoryRouter>
+          <Navbar />
+      </MemoryRouter>
+    );
+      expect(screen.getByText('About Us')).toBeInTheDocument()
+  });
+
+
 });
