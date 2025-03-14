d33ee545eca5ed94101f5c702e7557751945c115
T-Brex <bressan816@gmail.com>
2025-03-14 17:21:04 +0100

    Ritorno alla versione precedente(modificata da leo)

--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -9,184 +9,175 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
+import Alert from '@mui/material/Alert';
 import { styled } from '@mui/material/styles';
-import { useNavigate } from "react-router";
-import useAuth from "../../hooks/useAuth";
+import useAuth from "../../hooks/useAuth"
 import "../../index.css";
 
 const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: "100px",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
+  margin: '100px',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
   },
   boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
   //height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-  minHeight: "100%",
+  minHeight: '100%',
   padding: theme.spacing(2),
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(4),
   },
-  "&::before": {
+  '&::before': {
     content: '""',
-    display: "block",
-    position: "absolute",
+    display: 'block',
+    position: 'absolute',
     zIndex: -1,
     inset: 0,
     backgroundImage:
-      "radial-gradient(ellipse at 50% 50%, hsl(0, 6.10%, 90.40%), hsl(0, 0.00%, 100.00%))",
-    backgroundRepeat: "no-repeat",
+      'radial-gradient(ellipse at 50% 50%, hsl(0, 6.10%, 90.40%), hsl(0, 0.00%, 100.00%))',
+    backgroundRepeat: 'no-repeat',
   },
 }));
 
 export default function SignIn() {
-  let navigate = useNavigate();
   const { user, loginUser, error } = useAuth();
   const [usernameError, setUsernameError] = React.useState(false);
-  const [usernameErrorMessage, setUsernameErrorMessage] = React.useState("");
+  const [usernameErrorMessage, setUsernameErrorMessage] = React.useState('');
   const [passwordError, setPasswordError] = React.useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
-
-  //Quando error o user cambiano viene eseguito ciò che c'è di seguito
-  //se il corpo di React.useEffect viene messo sotto await loginUser(username, password) funziona al secondo submit.
-  React.useEffect(() => {
-    if (error) {
-      console.log(error);
-    }
-    if (!error && user) {
-      console.log(user);
-      console.log("dentro");
-      navigate("/");
-    }
-  }, [error, user]);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("handleSubmit");
     event.preventDefault();
     if (usernameError || passwordError) return;
-
+  
     const data = new FormData(event.currentTarget);
     const username = data.get("username") as string;
     const password = data.get("password") as string;
-
+  
     await loginUser(username, password);
   };
 
   const validateInputs = () => {
-    const username = document.getElementById("username") as HTMLInputElement;
-    const password = document.getElementById("password") as HTMLInputElement;
+    const username = document.getElementById('username') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
 
     let isValid = true;
 
     if (!username.value) {
       setUsernameError(true);
-      setUsernameErrorMessage("Please enter your username.");
+      setUsernameErrorMessage('Please enter your username.');
       isValid = false;
     } else {
       setUsernameError(false);
-      setUsernameErrorMessage("");
+      setUsernameErrorMessage('');
     }
 
     //  password.value.length < 6 --> per il sign up facciamo questo controllo
     if (!password.value) {
       setPasswordError(true);
-      setPasswordErrorMessage("Please enter your password.");
+      setPasswordErrorMessage('Please enter your password.');
       isValid = false;
     } else {
       setPasswordError(false);
-      setPasswordErrorMessage("");
+      setPasswordErrorMessage('');
     }
 
     return isValid;
   };
 
   return (
-    <SignInContainer direction="column" justifyContent="space-between">
-      <Card variant="elevation">
-        <Typography
-          component="h1"
-          variant="h4"
-          sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
-        >
-          Sign in
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            gap: 2,
-          }}
-        >
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <TextField
-              error={usernameError}
-              helperText={usernameErrorMessage}
-              id="username"
-              type="username"
-              name="username"
-              placeholder="your username"
-              autoComplete="username"
-              autoFocus
-              required
-              fullWidth
-              variant="outlined"
-              color={usernameError ? "error" : "primary"}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <TextField
-              error={passwordError}
-              helperText={passwordErrorMessage}
-              name="password"
-              placeholder="••••••"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              autoFocus
-              required
-              fullWidth
-              variant="outlined"
-              color={passwordError ? "error" : "primary"}
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            onClick={validateInputs}
-            sx={{ backgroundColor: "var(--maincolor)" }}
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Card variant="elevation">
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
           >
             Sign in
-          </Button>
-        </Box>
-        <Divider>or</Divider>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Typography sx={{ textAlign: "center" }}>
-            Don&apos;t have an account?{" "}
-            <Link
-              href="/signup"
-              variant="body2"
-              sx={{ alignSelf: "center", color: "var(--maincolor)" }}
-            >
-              Sign up
-            </Link>
           </Typography>
-        </Box>
-      </Card>
-    </SignInContainer>
+          {error !== null && 
+          <Alert severity="error">
+            {error.toString()}
+          </Alert>}
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <TextField
+                error={usernameError}
+                helperText={usernameErrorMessage}
+                id="username"
+                type="username"
+                name="username"
+                placeholder="your username"
+                autoComplete="username"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={usernameError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <TextField
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={validateInputs}
+              sx={{ backgroundColor: "var(--maincolor)"}}
+            >
+              Sign in
+            </Button>
+          </Box>
+          <Divider>or</Divider>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Typography sx={{ textAlign: 'center' }}>
+              Don&apos;t have an account?{' '}
+              <Link
+                href="/signup"
+                variant="body2"
+                sx={{ alignSelf: 'center', color: "var(--maincolor)"}}
+              >
+                Sign up
+              </Link>
+            </Typography>
+          </Box>
+        </Card>
+      </SignInContainer>
   );
 }
