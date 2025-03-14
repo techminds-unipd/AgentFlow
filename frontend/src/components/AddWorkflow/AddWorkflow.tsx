733c98fcb7391fa018dd1ba05d2741e7b3a9a7c2
cvallott <caterina.vallotto@studenti.unipd.it>
2025-03-14 16:36:36 +0100

    snackbar

--- frontend/src/components/AddWorkflow/AddWorkflow.tsx
+++ frontend/src/components/AddWorkflow/AddWorkflow.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField, IconButton, Snackbar, Typography, SnackbarCloseReason  } from "@mui/material";
+import { Box, TextField, IconButton, Snackbar, SnackbarCloseReason } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import CloseIcon from '@mui/icons-material/Close';
 import React from "react";
@@ -6,80 +6,72 @@ import "../../index.css";
 
 export const AddWorkflow = () => {
     const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState("");
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+    const handleClick = () => {
+        setMessage("Tha name you choose already exists.");
+        setOpen(true);
+    };
 
-  const handleClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: SnackbarCloseReason,
-  ) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
-    const action = (
-        <React.Fragment>
-          <IconButton
-            size="small"
-            aria-label="close"
-            color="inherit"
-            onClick={handleClose}
-          >
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        </React.Fragment>
-      );
+    const handleClose = (
+        event?: React.SyntheticEvent | Event,
+        reason?: SnackbarCloseReason
+    ) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
 
     return (
-    <>
-        <Box
-            component="section"
-            sx={{
-                p: 2,
-                borderRadius: 1,
-                backgroundColor: "var(--maincolor)",
-                display: "flex",
-                alignItems: "center",
-                gap: 1,
-                width: 750
-            }}
-        >
-            <TextField 
-                label="Insert workflow name" 
-                variant="outlined"
-                sx={{ 
-                    backgroundColor: "var(--white-text)", 
-                    borderRadius: 1, 
-                    flexGrow: 1,
-                    "& .MuiInputLabel-root": { 
-                        color: "gray"
-                    },
-                    "& .MuiInputLabel-root.Mui-focused": { 
-                        color: "gray"
-                    },
-                    "& .MuiFilledInput-root": {
-                        backgroundColor: "var(--white-text)",
-                        "&:hover": {
-                            backgroundColor: "var(--white-text)"
+        <>
+            <Box
+                component="section"
+                sx={{
+                    p: 2,
+                    borderRadius: 1,
+                    backgroundColor: "var(--maincolor)",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: 1,
+                    width: 750
+                }}
+            >
+                <TextField 
+                    label="Insert workflow name" 
+                    variant="outlined"
+                    sx={{ 
+                        backgroundColor: "var(--white-text)", 
+                        borderRadius: 1, 
+                        flexGrow: 1,
+                        "& .MuiInputLabel-root": { color: "gray" },
+                        "& .MuiInputLabel-root.Mui-focused": { color: "gray" },
+                        "& .MuiFilledInput-root": {
+                            backgroundColor: "var(--white-text)",
+                            "&:hover": { backgroundColor: "var(--white-text)" }
                         }
-                    }
-                }} 
-            />
-            <IconButton aria-label="Add workflow" size="large" onClick={handleClick}>
-                <Add />
-            </IconButton>
+                    }} 
+                />
+                <IconButton aria-label="Add workflow" size="large" onClick={handleClick}>
+                    <Add />
+                </IconButton>
+            </Box>
             <Snackbar
                 open={open}
                 autoHideDuration={6000}
                 onClose={handleClose}
-                message="Note archived"
-                action={action}
+                message={message}
+                action={
+                    <IconButton
+                        size="small"
+                        aria-label="close"
+                        color="inherit"
+                        onClick={handleClose}
+                    >
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                }
             />
-        </Box>
-    </>
-  );
+        </>
+    );
 };
