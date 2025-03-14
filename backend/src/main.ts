00148a7ae0d02dfb3d63ec8594d18837c373f17c
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-14 11:59:48 +0100

    Add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder, OpenAPIObject } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const config = new DocumentBuilder()
         .setTitle("Backend")
         .setDescription("Descrizione generica")
