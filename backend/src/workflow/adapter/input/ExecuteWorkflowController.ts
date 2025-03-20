7908ea39517e9fa1bd8a0395520615345bf71ea1
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2025-03-20 10:39:35 +0100

    mock altri metodi

--- backend/src/workflow/adapter/input/ExecuteWorkflowController.ts
+++ backend/src/workflow/adapter/input/ExecuteWorkflowController.ts
@@ -10,6 +10,7 @@ import { isNotIn } from "class-validator";
 import { HttpService } from "@nestjs/axios";
 import { catchError, firstValueFrom } from "rxjs";
 import WorkflowDTOValidator from "./WorkflowDTOValidator";
+import WorkflowAdapterImplementation from "./WorkflowAdapterImplementation";
 
 @ApiBearerAuth()
 @Controller("workflow")
@@ -17,34 +18,9 @@ class ExecuteWorkflowController {
     constructor(
         @Inject(EXECUTE_WORKFLOW_USE_CASE) private readonly executeWorkflowUseCase: ExecuteWorkflowUseCase,
         private readonly httpService: HttpService,
-        private readonly workflowDTOValidator: WorkflowDTOValidator
+        private readonly workflowDTOValidator: WorkflowDTOValidator,
+        private readonly workflowAdapterImplementation: WorkflowAdapterImplementation
     ) {}
-
-    // uguale ad altri
-    /*eslint-disable max-statements*/
-    private toDomain(workflowDto: WorkflowDTO): Workflow {
-        const workflow: Workflow = new Workflow(workflowDto.name, []);
-        let firstNode = undefined;
-        const target = workflowDto.edges.map((edge) => edge.target);
-        for (let i = 0; i < workflowDto.nodes.length && !firstNode; i++)
-            if (isNotIn(workflowDto.nodes[i].id, target)) firstNode = workflowDto.nodes[i];
-        let nextNode = firstNode;
-        let edge = workflowDto.edges.find((edge) => edge.source === nextNode!.id);
-        workflow.nodes.push(
-            new Node(nextNode!.data.label as NodeType, edge!.label, new Point(nextNode!.position.x, nextNode!.position.y))
-        );
-        let action: string;
-        for (let i = 0; i < workflowDto.nodes.length - 1; i++) {
-            nextNode = workflowDto.nodes.find((node) => node.id === edge!.target);
-            edge = workflowDto.edges.find((edge) => edge.source === nextNode!.id);
-            if (edge) action = edge.label;
-            else action = "";
-            workflow.nodes.push(
-                new Node(nextNode!.data.label as NodeType, action, new Point(nextNode!.position.x, nextNode!.position.y))
-            );
-        }
-        return workflow;
-    }
 
     private async validate(accessToken: string): Promise<void> {
         await firstValueFrom(
@@ -64,7 +40,7 @@ class ExecuteWorkflowController {
     async executeWorkflow(@Body() executeReq: ExecuteWorkflowDTO): Promise<string> {
         this.workflowDTOValidator.validate(executeReq.workflow);
         await this.validate(executeReq.googleToken.token);
-        const workflow = this.toDomain(executeReq.workflow);
+        const workflow = this.workflowAdapterImplementation.toDomain(executeReq.workflow);
         const cmd = new ExecuteWorkflowCommand(workflow, executeReq.googleToken);
         try {
             return await this.executeWorkflowUseCase.executeWorkflow(cmd);
--- backend/src/workflow/adapter/input/GetWorkflowController.ts
+++ backend/src/workflow/adapter/input/GetWorkflowController.ts
@@ -6,22 +6,16 @@ import GetWorkflowCommand from "src/workflow/domain/GetWorkflowCommand";
 import { AuthGuard } from "./AuthGuard";
 import { Workflow } from "src/workflow/domain/Workflow";
 import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
+import WorkflowAdapterImplementation from "./WorkflowAdapterImplementation";
 
 @ApiBearerAuth()
 @Controller("workflow")
 class GetWorkflowController {
-    constructor(@Inject(GET_WORKFLOW_USE_CASE) private readonly getWorkflowUseCase: GetWorkflowUseCase) {}
-
-    private toDTO(workflow: Workflow): WorkflowDTO {
-        const nodes: NodeDTO[] = workflow.nodes.map(
-            (node, index) => new NodeDTO(index, new PositionDTO(node.position.x, node.position.y), new NodeDataDTO(node.type))
-        );
-        const edges: EdgeDTO[] = workflow.nodes
-            .slice(0, workflow.nodes.length - 1)
-            .map((node, index) => new EdgeDTO(node.action, index, index + 1));
-
-        return new WorkflowDTO(workflow.name, nodes, edges);
-    }
+    constructor(
+        @Inject(GET_WORKFLOW_USE_CASE) 
+        private readonly getWorkflowUseCase: GetWorkflowUseCase,
+        private readonly workflowAdapterImplementation: WorkflowAdapterImplementation
+    ) {}
 
     @UseGuards(AuthGuard)
     @Get("/get/:name")
@@ -32,7 +26,7 @@ class GetWorkflowController {
         try {
             const username = request.username;
             const workflow = await this.getWorkflowUseCase.getWorkflow(new GetWorkflowCommand(username, workflowName));
-            return this.toDTO(workflow);
+            return this.workflowAdapterImplementation.toDTO(workflow);
         } catch (error) {
             if (error instanceof WorkflowNotFoundError) throw new HttpException("Workflow not found", HttpStatus.NOT_FOUND);
 
