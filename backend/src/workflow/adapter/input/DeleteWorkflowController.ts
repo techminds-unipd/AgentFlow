52de44f723a3bd9e837dc4b3e1fc531d86165497
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2025-03-20 11:06:52 +0100

    unit testing WorkflowAdapterImplementation

--- backend/src/workflow/adapter/input/DeleteWorkflowController.ts
+++ backend/src/workflow/adapter/input/DeleteWorkflowController.ts
@@ -6,22 +6,16 @@ import { DeleteWorkflowUseCase, DELETE_WORKFLOW_USE_CASE } from "src/workflow/se
 import DeleteWorkflowCommand from "src/workflow/domain/DeleteWorkflowCommand";
 import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
 import { WorkflowNotFoundError } from "src/BusinessErrors";
+import WorkflowAdapterImplementation from "./WorkflowAdapterImplementation";
 
 @ApiBearerAuth()
 @Controller("workflow")
 class DeleteWorkflowController {
-    constructor(@Inject(DELETE_WORKFLOW_USE_CASE) private readonly deletetWorkflowUseCase: DeleteWorkflowUseCase) {}
-
-    private toDTO(workflow: Workflow): WorkflowDTO {
-        const nodes: NodeDTO[] = workflow.nodes.map(
-            (node, index) => new NodeDTO(index, new PositionDTO(node.position.x, node.position.y), new NodeDataDTO(node.type))
-        );
-        const edges: EdgeDTO[] = [];
-
-        for (let i = 0; i < workflow.nodes.length - 1; i++) edges.push(new EdgeDTO(workflow.nodes[i].action, i, i + 1));
-
-        return new WorkflowDTO(workflow.name, nodes, edges);
-    }
+    constructor(
+        @Inject(DELETE_WORKFLOW_USE_CASE) 
+        private readonly deletetWorkflowUseCase: DeleteWorkflowUseCase,
+        private readonly workflowAdapterImplementation: WorkflowAdapterImplementation
+    ) {}
 
     @UseGuards(AuthGuard)
     @Delete("/delete/:name")
@@ -32,7 +26,7 @@ class DeleteWorkflowController {
         try {
             const username = request.username;
             const workflow = await this.deletetWorkflowUseCase.deleteWorkflow(new DeleteWorkflowCommand(username, workflowName));
-            return this.toDTO(workflow);
+            return this.workflowAdapterImplementation.toDTO(workflow);
         } catch (error) {
             if (error instanceof WorkflowNotFoundError) throw new HttpException("Workflow not found", HttpStatus.NOT_FOUND);
             throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
