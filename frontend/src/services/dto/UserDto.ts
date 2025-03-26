7b221bb2385c84209816060c1e7d1dd4328d74c5
cvallott <caterina.vallotto@studenti.unipd.it>
2025-03-26 21:25:56 +0100

    eliminato userDto

--- frontend/src/services/dto/UserDto.ts
+++
@@ -1,9 +0,0 @@
-export class PositionDTO {
-    readonly username: string;
-    readonly password: string;
-
-    constructor(username: string, password: string) {
-        this.username = username;
-        this.password = password;
-    }
-}
