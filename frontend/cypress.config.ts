ba81c012bd752df4ff89c26438bc136bdc437ca8
Samuele <corradinsamuele@gmail.com>
2025-03-27 13:55:34 +0100

    Configurato file .env

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { defineConfig } from "cypress";
 
-export default defineConfig({ e2e: { baseUrl: "http://localhost:5173", viewportWidth: 1920, viewportHeight: 1080 } });
+export default defineConfig({ 
+    e2e: { baseUrl: "http://localhost:5173", viewportWidth: 1920, viewportHeight: 1080 },
+    env: {
+        googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        googleClientId: process.env.CLIENT_ID,
+        googleClientSecret: process.env.CLIENT_SECRET
+    }
+});
