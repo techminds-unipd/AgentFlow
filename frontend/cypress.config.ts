d4083fda28758f36e7f285b60c6b990a052d3870
Matteo Squarzoni <matteo.squarzoni03@gmail.com>
2025-03-29 10:47:51 +0100

    Comando per runnare tutti i test in una singola volta

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 import { defineConfig } from "cypress";
 
 export default defineConfig({ 
-    e2e: { baseUrl: "http://localhost:5173", viewportWidth: 1920, viewportHeight: 1080 },
+    e2e: { baseUrl: "http://localhost:5173", viewportWidth: 1920, viewportHeight: 1080, experimentalRunAllSpecs: true },
     env: {
         googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
         googleClientId: process.env.CLIENT_ID,
