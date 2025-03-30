9486d4142341ad0019783e4774929896e4fb39e0
leosalvi03 <leonardosalviato03@gmail.com>
2025-03-30 17:40:03 +0200

    Aggiunta then dopo visit #85 #106

--- frontend/cypress/e2e/ts47_ts51_ts53_ts58_esecuzione_workflow.cy.ts
+++ frontend/cypress/e2e/ts47_ts51_ts53_ts58_esecuzione_workflow.cy.ts
@@ -1,6 +1,13 @@
 import { EdgeDTO, NodeDataDTO, NodeDTO, WorkflowDTO } from "../../src/services/dto/WorkflowDTO";
 describe("Esecuzione workflow TS47-TS51 e TS53-TS58", () => {
-    const workflow = new WorkflowDTO("test", [new NodeDTO(0, { x: -200, y: 1 }, new NodeDataDTO("Gmail")), new NodeDTO(1, { x: 200, y: 1 }, new NodeDataDTO("GCalendar"))], [new EdgeDTO("Get the last email from Tullio Vardanega and create an event", 0, 1)]);
+    const workflow = new WorkflowDTO(
+        "test",
+        [
+            new NodeDTO(0, { x: -200, y: 1 }, new NodeDataDTO("Gmail")),
+            new NodeDTO(1, { x: 200, y: 1 }, new NodeDataDTO("GCalendar"))
+        ],
+        [new EdgeDTO("Get the last email from Tullio Vardanega and create an event", 0, 1)]
+    );
     beforeEach(() => {
         cy.registerIfNotExistsAPI(Cypress.env("loginUsername"), Cypress.env("loginPassword"));
         cy.loginUISession(Cypress.env("loginUsername"), Cypress.env("loginPassword"));
@@ -8,37 +15,46 @@ describe("Esecuzione workflow TS47-TS51 e TS53-TS58", () => {
         cy.createWorkflowAPI("test");
         cy.saveWorkflowAPI(workflow);
         cy.loginByGoogleApi();
-        cy.visit("/workflow/test");
     });
 
     it("TS47 - L’utente autenticato può mandare in esecuzione un workflow", () => {
-        cy.get("[data-cy=execute-button]").should("be.visible");
+        cy.visit("/workflow/test").then(() => {
+            cy.get("[data-cy=execute-button]").should("be.visible");
+        });
     });
 
     it("TS48 - L’utente autenticato, dopo aver mandato in esecuzione un workflow con una struttura non valida, visualizza un messaggio d’errore", () => {
-        cy.get("[data-cy='delete-source-target-node-0-1']").click();
-        cy.get("[data-cy=execute-button]").click()
-        cy.get("[data-cy='snackbar-workflow'").should("contain", "Cannot execute the workflow");
+        cy.visit("/workflow/test").then(() => {
+            cy.get("[data-cy='delete-source-target-node-0-1']").click();
+            cy.get("[data-cy=execute-button]")
+                .click()
+                .then(() => {
+                    cy.get("[data-cy='snackbar-workflow'").should("contain", "Cannot execute the workflow");
+                });
+        });
     });
 
     it("TS49 - L’utente autenticato visualizza un messaggio che lo informa di un errore runtime nell’esecuzione del workflow", () => {
-        cy.intercept(
-            'POST',
-            'http://localhost:3000/workflow/execute',
-            { statusCode: 500 }
-        ).as('getServerFailure')
-        cy.get("[data-cy=execute-button]").click()
-        cy.get("[data-cy='snackbar-workflow'").should("contain", "Cannot execute the workflow");
+        cy.visit("/workflow/test").then(() => {
+            cy.intercept("POST", "http://localhost:3000/workflow/execute", { statusCode: 500 }).as("getServerFailure");
+            cy.get("[data-cy=execute-button]")
+                .click()
+                .then(() => {
+                    cy.get("[data-cy='snackbar-workflow'").should("contain", "Cannot execute the workflow");
+                });
+        });
     });
 
     it("TS50-TS51-TS53-TS54-TS55-TS56-TS57-TS58 - L’utente autenticato può visualizzare il risultato dell’esecuzione del workflow", () => {
-        cy.wait(500);
-        cy.get("[data-cy=execute-button]").click()
-        cy.wait(10000);
-        cy.get("[data-cy=execute-result-dialog]").should("be.visible");
-        cy.get("[data-cy=execute-result-dialog-content]").should("be.visible");
-        // TS51
-        cy.get("[data-cy=execute-result-dialog-content]").contains("ACTION:");
-        cy.get("[data-cy=execute-result-dialog-content]").contains("AI:");
+        cy.visit("/workflow/test").then(() => {
+            cy.wait(500);
+            cy.get("[data-cy=execute-button]").click();
+            cy.wait(10000);
+            cy.get("[data-cy=execute-result-dialog]").should("be.visible");
+            cy.get("[data-cy=execute-result-dialog-content]").should("be.visible");
+            // TS51
+            cy.get("[data-cy=execute-result-dialog-content]").contains("ACTION:");
+            cy.get("[data-cy=execute-result-dialog-content]").contains("AI:");
+        });
     });
 });
